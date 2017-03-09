d48c39f04512e354c5815d278f5d3bf536341975
Bharath Rao <bharath.rao@gmail.com>
2017-03-08 19:50:41 -0800

    change theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -84,13 +84,19 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
 # html_theme_options = {}
+html_theme_options = {
+    'collapse_navigation': False,
+    'display_version': False,
+    'navigation_depth': 3,
+}
+html_theme_options['canonical_url'] = 'https://coinpit.io/docs/latest/'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -153,6 +159,3 @@ texinfo_documents = [
      author, 'coinpit', 'One line description of project.',
      'Miscellaneous'),
 ]
-
-
-
